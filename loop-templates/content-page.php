4493847a2f2eed9e9ab196612009ba584e4395a0
Danijel J <test@test.com>
2022-08-25 02:53:10 +0200

    update

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -12,21 +12,16 @@ defined( 'ABSPATH' ) || exit;
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 	<header class="entry-header">
-
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
 	</header><!-- .entry-header -->
 
 	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
-	<div class="entry-content">
+	<div class="container entry-content">
 		<div class="gh--page-content">
 			<?php
 				the_content();
 			?>
 		</div>
-
-
-	</div><!-- .entry-content -->
+	</div>
 
 </article><!-- #post-## -->
