de749bb21d4b8989a05460dcef9b765a39fa9692
Danijel J <test@test.com>
2022-08-26 14:14:52 +0200

    js

--- js/theme.js
+++ js/theme.js
@@ -9297,12 +9297,18 @@
 	})();
 
 	jQuery(document).ready(function ($) {
-	  $(window).scroll(function () {
-	    if ($(this).scrollTop() > 1500) {
-	      $('body').addClass('move-on');
-	    } else {
-	      $('body').removeClass('move-on');
-	    }
+	  // $(window).scroll(function(){
+	  //     if ($(this).scrollTop() > 1500) {
+	  //        $('body').addClass('move-on');
+	  //     } else {
+	  //        $('body').removeClass('move-on');
+	  //     }
+	  // });
+	  $('.entry-content').waypoint({
+	    handler: function (direction) {
+	      console.log('hit');
+	    },
+	    offset: '100%'
 	  });
 	});
 
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -4,11 +4,16 @@ import $ from 'jquery';
 
 
 jQuery(document).ready(function($) {
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 1500) {
-           $('body').addClass('move-on');
-        } else {
-           $('body').removeClass('move-on');
-        }
-    });
+    // $(window).scroll(function(){
+    //     if ($(this).scrollTop() > 1500) {
+    //        $('body').addClass('move-on');
+    //     } else {
+    //        $('body').removeClass('move-on');
+    //     }
+    // });
+    $('.entry-content').waypoint({
+        handler: function(direction) {
+            console.log('hit');
+        },
+    offset: '100%'});
 })
